ccff1bd4106ce12aa4634e0609e83ff1b7bfe115
unknown <shameemkhanam@gmail.com>
2022-12-18 22:50:34 +0530

    18/12/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HighlightDirective } from './customDirectives/highlight.directive';
 import { HoverDirective } from './customDirectives/hover.directive';
 import { BetterhighlightDirective } from './customDirectives/betterhighlight.directive';
 import { ClassDirective } from './customDirectives/class.directive';
+import { StyleDirective } from './customDirectives/style.directive';
 
 @NgModule({
-  declarations: [AppComponent, SetBackgroundDirective, HighlightDirective, HoverDirective, BetterhighlightDirective, ClassDirective],
+  declarations: [AppComponent, SetBackgroundDirective, HighlightDirective, HoverDirective, BetterhighlightDirective, ClassDirective, StyleDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
